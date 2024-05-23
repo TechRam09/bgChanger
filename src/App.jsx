30bf1c9976e17f0be80f4c88590045a3c3ec1f8c
Shreeram N S <nsshreeram@gmail.com>
2024-05-24 03:20:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [color, setcolor] = useState('black')
+  const [color, setColor] = useState('black')
 
   return (
-    <div className=' h-screen w-full duration-200 flex justify-center' style={{backgroundColor:color}}>
-      <div className="  rounded fixed bottom-12 flex justify-center inset-x-0 flex-wrap items-center" style={{backgroundColor:'white'}}></div>
-
+    <div className=' h-screen w-full duration-200 ' style={{backgroundColor:color}}>
+     
     </div>
   )
 }
