6a7aafb2d7e14f20b567a2508f0120b746eb3537
Shreeram N S <nsshreeram@gmail.com>
2024-05-24 03:21:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ function App() {
 
   return (
     <div className=' h-screen w-full duration-200 ' style={{backgroundColor:color}}>
-     
+      <div className=" inset-x-0 p-5 flex flex-wrap m-5 fixed bottom-5 rounded-xl justify-center " >
+        
+      </div>
     </div>
   )
 }
